9560302f4191ade96d5e979baa42ec6f6c88dfce
skaur93 <svirk309@gmail.com>
2023-02-21 14:36:48 -0500

    react infinite scroll

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 export default class App extends Component {
   name='sandy'
   render() {
     return (
       <div>
-        Hi {this.name}
-        <Navbar></Navbar>
-        <News pageSize="5" country="in" category="sports"></News>
+        <BrowserRouter>
+          Hi {this.name}
+          <Navbar></Navbar>
+          <Routes>
+            <Route  exact path="/"  element={<News  key="general"  pageSize={6} country="in" category="general"/>} />
+            <Route exact path="/general"   element={<News  key="general" pageSize={6} country="in" category="general"/>} />
+            <Route exact path="/business"   element={<News key="business"  pageSize={6} country="in" category="business"/>} />
+            <Route  exact path="/health"  element={<News  key="health" pageSize={6} country="in" category="health"/>} />
+            <Route  exact path="/sports" element={<News  key="sports"  pageSize={6} country="in" category="sports"/>} />
+            <Route exact path="/entertainment" element={<News  key="entertainment"   pageSize={6} country="in" category="entertainment"/>} />
+            <Route  exact path="/sports" element={<News key="sports"  pageSize={6} country="in" category="sports"/>} />
+            <Route  exact path="/science" element={<News key="science"  pageSize={6} country="in" category="science"/>} />
+        </Routes>
+        </BrowserRouter>
       </div>
     )
   }
